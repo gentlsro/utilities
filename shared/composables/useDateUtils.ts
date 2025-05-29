8bf97d08ae9a065f9cabdca17454cd6b91e255bd
Kylian, Jakub <jacob.kylian@gmail.com>
2025-05-29 13:15:18 +0200

    up

--- shared/composables/useDateUtils.ts
+++ shared/composables/useDateUtils.ts
@@ -23,6 +23,13 @@ export type IDateOptions = {
   isLocalString?: boolean
   outputIntlOptions?: Intl.DateTimeFormatOptions | DateFormatPreset
   format?: string
+
+  /**
+   * When true, the spaces between the date parts will be removed
+   * For exmaple, for czech locale, the date would be formatted as `29. 05. 2025` by default
+   * but with `removeSpaces: true` it would be formatted as `29.05.2025`
+   */
+  removeSpaces?: boolean
 }
 
 export type DateFormatPreset = keyof typeof datetimeFormats
