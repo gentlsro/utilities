53fac1048188c968df4906b3131ba1f4487f8c08
Kylian, Jakub <jacob.kylian@gmail.com>
2025-07-04 23:10:24 +0200

    up

--- shared/types/tree-node.type.ts
+++ shared/types/tree-node.type.ts
@@ -1,19 +1,3 @@
 export type ITreeNode<T extends IItem = IItem> = IItem<T> & {
-  /**
-   * Unique ID of the node
-   */
   id: string | number
-
-  /**
-   * Name of the node
-   */
-  name?: string
-
-  /**
-   * Children of the node
-   *
-   * NOTE: You should use empty array instead of `undefined` if you don't want
-   * the item to have the "expand" button
-   */
-  children?: ITreeNode<T>[]
 }
