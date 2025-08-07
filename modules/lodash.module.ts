18889d6d72742a4a54f76e46c27b743068662f93
Kylian, Jakub <jacob.kylian@gmail.com>
2025-08-07 23:03:59 +0200

    up

---
+++ modules/lodash.module.ts
@@ -0,0 +1,40 @@
+import { addImports, createResolver, defineNuxtModule } from 'nuxt/kit'
+import * as lodash from 'lodash-es'
+
+const EXCLUDED_KEYS = [
+  'wrapperValue',
+  'wrapperToIterator',
+  'wrapperReverse',
+  'wrapperPlant',
+  'wrapperNext',
+  'wrapperLodash',
+  'wrapperCommit',
+  'wrapperChain',
+  'wrapperAt',
+  'templateSettings',
+  'toIterator',
+  'VERSION',
+  'lodash',
+  'value',
+  'valueOf',
+  'toJSON',
+  'thru',
+  'plant',
+  'next',
+  'default',
+  'commit',
+  'head',
+]
+
+export default defineNuxtModule({
+  setup: () => {
+    console.log('🚀 Lodash module setup')
+    for (const name of Object.keys(lodash)) {
+      if (EXCLUDED_KEYS.includes(name)) {
+        continue
+      }
+
+      addImports({ name, as: name, from: 'lodash-es' })
+    }
+  },
+})
