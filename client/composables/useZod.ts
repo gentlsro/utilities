489e1b32163cd6354b23ce9e04c85e1cf641f8be
Kylian, Jakub <jacob.kylian@gmail.com>
2025-10-30 12:18:16 +0100

    up

--- client/composables/useZod.ts
+++ client/composables/useZod.ts
@@ -420,7 +420,7 @@ export function useZod<T extends ZodSchemaObject>(
     false, // Leading
   )
 
-  const { pause, resume } = watch(
+  const { pause, resume } = watchPausable(
     dataReactive,
     debouncedValidate,
     { deep, immediate },
