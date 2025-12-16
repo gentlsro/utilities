958847cc26b9d5f94bbdd2bde4568e6cb8d26c17
Kylian, Jakub <jacob.kylian@gmail.com>
2025-12-16 17:25:35 +0100

    up

--- client/functions/make-options-from-enum.ts
+++ client/functions/make-options-from-enum.ts
@@ -7,9 +7,7 @@ export function makeSelectorOptionsFromEnum(
   options?: {
     keyField?: string
     labelField?: string
-
     transformKey?: (key: string) => string
-
     /**
      * If set to true, the value will be parsed to integer
      */
@@ -23,24 +21,42 @@ export function makeSelectorOptionsFromEnum(
     transformKey = (key: string) => key,
   } = options || {}
 
-  const enumKeys = Object.keys(enumObj).slice(0, Object.keys(enumObj).length / 2)
-  const enumValues = Object.values(enumObj).slice(0, Object.values(enumObj).length / 2)
-  const isNumberedEnum = enumKeys.every(key => /^[\d.]+$/.test(key))
+  // Get all keys and values
+  const allKeys = Object.keys(enumObj)
+  const allValues = Object.values(enumObj)
 
-  if (isNumberedEnum) {
-    return enumKeys
-      .map(value => {
-        return {
-          [labelField]: $t(`${translationPrefix}.${value}`),
-          [keyField]: numericValue ? Number.parseInt(value) : transformKey(value),
-        }
-      })
+  // Check if this is a numeric enum by testing if values are numbers
+  // Numeric enums have reverse mappings, so we need to filter out numeric keys
+  const isNumericEnum = allValues.some(val => typeof val === 'number')
+
+  let enumKeys: string[]
+  let enumValues: any[]
+
+  if (isNumericEnum) {
+    // For numeric enums, filter out numeric keys (keep only string keys which are the enum member names)
+    enumKeys = allKeys.filter(key => !/^\d+$/.test(key))
+    enumValues = enumKeys.map(key => enumObj[key])
   } else {
-    return enumKeys.map((_, idx) => {
-      const value = enumValues[idx]
+    // For string enums, use all keys (no reverse mapping exists)
+    enumKeys = allKeys
+    enumValues = enumKeys.map(key => enumObj[key])
+  }
 
+  console.log({ enumKeys, enumValues, isNumericEnum })
+
+  if (isNumericEnum) {
+    return enumKeys.map(key => {
+      const value = enumObj[key]
       return {
-        [labelField]: $t(`${translationPrefix}.${transformKey(value)}`),
+        [labelField]: $t(`${translationPrefix}.${transformKey(key)}`),
+        [keyField]: numericValue ? Number.parseInt(String(value)) : transformKey(String(value)),
+      }
+    })
+  } else {
+    return enumKeys.map(key => {
+      const value = enumObj[key]
+      return {
+        [labelField]: $t(`${translationPrefix}.${transformKey(String(value))}`),
         [keyField]: value,
       }
     })
