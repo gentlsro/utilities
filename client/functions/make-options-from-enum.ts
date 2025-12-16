e12a562acd033339ebf90315b353c51588a73814
Kylian, Jakub <jacob.kylian@gmail.com>
2025-12-16 14:41:11 +0100

    up

--- client/functions/make-options-from-enum.ts
+++ client/functions/make-options-from-enum.ts
@@ -24,7 +24,7 @@ export function makeSelectorOptionsFromEnum(
   } = options || {}
 
   const enumValues = Object.values(enumObj)
-  const isNumberedEnum = enumValues.some(key => Number.isFinite(Number(key)))
+  const isNumberedEnum = enumValues.every(key => Number.isFinite(Number(key)))
 
   if (isNumberedEnum) {
     return enumValues
