06334247f6c66b360842d53386659577f5fd648e
Jakub Kylian <jacob.kylian@gmail.com>
2026-01-10 17:48:07 +0100

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,25 +6,9 @@ const isMonorepo = import.meta.env.VITE_MONOREPO === 'true'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [],
-
-  // Modules https://nuxt.com/docs/api/configuration/nuxt-config#modules
-  modules: [
-    '@vueuse/nuxt',
-    '@nuxtjs/i18n',
-    'dayjs-nuxt',
-  ],
-
-  // Layer meta
-  $meta: {
-    name: 'utilities',
-  },
-
   // Imports https://nuxt.com/docs/api/configuration/nuxt-config#imports
   imports: {
     imports: [
-      { name: 'z', from: 'zod/v4' },
-
       // Client
       { name: 'getComponentName', from: resolve('./app/functions/get-component-name.ts') },
       { name: '$t', from: resolve('./app/functions/$t.ts') },
@@ -49,26 +33,10 @@ export default defineNuxtConfig({
     ],
   },
 
-  // Devtools https://nuxt.com/docs/api/configuration/nuxt-config#devtools
-  devtools: {
-    enabled: isMonorepo,
-  },
-
-  // Alias
-  alias: {
-    $utils: join(process.cwd(), 'generated', 'utils.ts'),
-    $utilsConfig: join(process.cwd(), 'generated', 'utilsConfig.ts'),
-    $comparatorEnum: join(process.cwd(), 'generated', 'comparator-enum.ts'),
-    $dataType: join(process.cwd(), 'generated', 'data-type.type.ts'),
-    $components: join(process.cwd(), 'generated', 'components-by-name.ts'),
-  },
-
   // Nitro https://nuxt.com/docs/api/configuration/nuxt-config#nitro
   nitro: {
     imports: {
       imports: [
-        { name: 'z', from: 'zod/v4' },
-
         // Lodash
         { name: 'get', from: 'lodash-es' },
         { name: 'set', from: 'lodash-es' },
@@ -82,69 +50,10 @@ export default defineNuxtConfig({
         { name: '$t', from: resolve('./server/functions/$t.ts') },
 
         // Shared
-        { name: 'generateUUID', from: resolve('./shared/functions/generate-uuid.ts') },
-        { name: '$date', from: resolve('./server/functions/dayjs.ts') },
-        { name: '$duration', from: resolve('./server/functions/dayjs.ts') },
-        { name: '$log', from: resolve('./shared/functions/$log.ts') },
-        { name: 'IItem', from: resolve('./shared/types/item.type.ts'), type: true },
-        { name: 'ClassType', from: resolve('./app/types/class.type.ts'), type: true },
-        { name: 'Datetime', from: resolve('./shared/types/datetime.type.ts'), type: true },
-        { name: 'isDev', from: resolve('./shared/functions/is-dev.ts') },
 
         // Config
         { name: 'extendUtilitiesConfig', from: resolve('./config.ts') },
       ],
     },
-
-    alias: {
-      $utils: join(process.cwd(), 'generated', 'utils.ts'),
-      $utilsConfig: join(process.cwd(), 'generated', 'utilsConfig.ts'),
-      $comparatorEnum: join(process.cwd(), 'generated', 'comparator-enum.ts'),
-      $dataType: join(process.cwd(), 'generated', 'data-type.type.ts'),
-    },
-  },
-
-  // Dayjs
-  dayjs: {
-    defaultLocale: 'en-gb',
-    locales: ['en-gb', 'sr', 'cs'],
-    plugins: [
-      'duration',
-      'customParseFormat',
-      'isBetween',
-      'isSameOrAfter',
-      'isSameOrBefore',
-      'isoWeek',
-      'dayOfYear',
-      'utc',
-      'timezone',
-      'quarterOfYear',
-    ],
-  },
-
-  i18n: {
-    langDir: '../i18n',
-    compilation: {
-      strictMessage: false,
-      escapeHtml: true,
-    },
-    locales: [
-      {
-        code: 'en-US',
-        file: 'en-US_utilities.json',
-        dateFormat: 'MM/DD/YYYY',
-        currency: 'USD',
-        language: 'English',
-        icon: 'i-emojione:flag-for-united-kingdom',
-      },
-      {
-        code: 'cs-CZ',
-        file: 'cs-CZ_utilities.json',
-        dateFormat: 'DD.MM.YYYY',
-        currency: 'CZK',
-        language: 'Česky',
-        icon: 'i-emojione:flag-for-czechia',
-      },
-    ],
   },
 })
