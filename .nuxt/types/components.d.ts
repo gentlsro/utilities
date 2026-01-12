fcf3b14f5ad0b66f8f76cb98509bf259840f72c1
Kylian, Jakub <jacob.kylian@gmail.com>
2026-01-12 23:12:36 +0100

    chore: up

---
+++ .nuxt/types/components.d.ts
@@ -0,0 +1,121 @@
+
+import type { DefineComponent, SlotsType } from 'vue'
+type IslandComponent<T> = DefineComponent<{}, {refresh: () => Promise<void>}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, SlotsType<{ fallback: { error: unknown } }>> & T
+
+type HydrationStrategies = {
+  hydrateOnVisible?: IntersectionObserverInit | true
+  hydrateOnIdle?: number | true
+  hydrateOnInteraction?: keyof HTMLElementEventMap | Array<keyof HTMLElementEventMap> | true
+  hydrateOnMediaQuery?: string
+  hydrateAfter?: number
+  hydrateWhen?: boolean
+  hydrateNever?: true
+}
+type LazyComponent<T> = DefineComponent<HydrationStrategies, {}, {}, {}, {}, {}, {}, { hydrated: () => void }> & T
+
+interface _GlobalComponents {
+  'ScriptGoogleMaps': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMaps.vue").default
+  'ScriptGoogleMapsAdvancedMarkerElement': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsAdvancedMarkerElement.vue").default
+  'ScriptGoogleMapsCircle': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsCircle.vue").default
+  'ScriptGoogleMapsHeatmapLayer': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsHeatmapLayer.vue").default
+  'ScriptGoogleMapsInfoWindow': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsInfoWindow.vue").default
+  'ScriptGoogleMapsMarker': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsMarker.vue").default
+  'ScriptGoogleMapsMarkerClusterer': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsMarkerClusterer.vue").default
+  'ScriptGoogleMapsPinElement': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPinElement.vue").default
+  'ScriptGoogleMapsPolygon': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPolygon.vue").default
+  'ScriptGoogleMapsPolyline': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPolyline.vue").default
+  'ScriptGoogleMapsRectangle': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsRectangle.vue").default
+  'ScriptAriaLoadingIndicator': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptAriaLoadingIndicator.vue").default
+  'ScriptCarbonAds': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptCarbonAds.vue").default
+  'ScriptCrisp': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptCrisp.vue").default
+  'ScriptGoogleAdsense': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptGoogleAdsense.vue").default
+  'ScriptIntercom': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptIntercom.vue").default
+  'ScriptLemonSqueezy': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptLemonSqueezy.vue").default
+  'ScriptLoadingIndicator': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptLoadingIndicator.vue").default
+  'ScriptPayPalButtons': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalButtons.vue").default
+  'ScriptPayPalMarks': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalMarks.vue").default
+  'ScriptPayPalMessages': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalMessages.vue").default
+  'ScriptStripePricingTable': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptStripePricingTable.vue").default
+  'ScriptVimeoPlayer': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptVimeoPlayer.vue").default
+  'ScriptYouTubePlayer': typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptYouTubePlayer.vue").default
+  'NuxtWelcome': typeof import("../../../../node_modules/nuxt/dist/app/components/welcome.vue").default
+  'NuxtLayout': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-layout").default
+  'NuxtErrorBoundary': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-error-boundary.vue").default
+  'ClientOnly': typeof import("../../../../node_modules/nuxt/dist/app/components/client-only").default
+  'DevOnly': typeof import("../../../../node_modules/nuxt/dist/app/components/dev-only").default
+  'ServerPlaceholder': typeof import("../../../../node_modules/nuxt/dist/app/components/server-placeholder").default
+  'NuxtLink': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-link").default
+  'NuxtLoadingIndicator': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-loading-indicator").default
+  'NuxtTime': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-time.vue").default
+  'NuxtRouteAnnouncer': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-route-announcer").default
+  'NuxtImg': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-stubs").NuxtImg
+  'NuxtPicture': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-stubs").NuxtPicture
+  'NuxtLinkLocale': typeof import("../../../../node_modules/@nuxtjs/i18n/dist/runtime/components/NuxtLinkLocale").default
+  'SwitchLocalePathLink': typeof import("../../../../node_modules/@nuxtjs/i18n/dist/runtime/components/SwitchLocalePathLink").default
+  'NuxtPage': typeof import("../../../../node_modules/nuxt/dist/pages/runtime/page-placeholder").default
+  'NoScript': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").NoScript
+  'Link': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Link
+  'Base': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Base
+  'Title': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Title
+  'Meta': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Meta
+  'Style': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Style
+  'Head': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Head
+  'Html': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Html
+  'Body': typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Body
+  'NuxtIsland': typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-island").default
+  'LazyScriptGoogleMaps': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMaps.vue").default>
+  'LazyScriptGoogleMapsAdvancedMarkerElement': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsAdvancedMarkerElement.vue").default>
+  'LazyScriptGoogleMapsCircle': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsCircle.vue").default>
+  'LazyScriptGoogleMapsHeatmapLayer': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsHeatmapLayer.vue").default>
+  'LazyScriptGoogleMapsInfoWindow': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsInfoWindow.vue").default>
+  'LazyScriptGoogleMapsMarker': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsMarker.vue").default>
+  'LazyScriptGoogleMapsMarkerClusterer': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsMarkerClusterer.vue").default>
+  'LazyScriptGoogleMapsPinElement': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPinElement.vue").default>
+  'LazyScriptGoogleMapsPolygon': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPolygon.vue").default>
+  'LazyScriptGoogleMapsPolyline': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsPolyline.vue").default>
+  'LazyScriptGoogleMapsRectangle': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/GoogleMaps/ScriptGoogleMapsRectangle.vue").default>
+  'LazyScriptAriaLoadingIndicator': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptAriaLoadingIndicator.vue").default>
+  'LazyScriptCarbonAds': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptCarbonAds.vue").default>
+  'LazyScriptCrisp': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptCrisp.vue").default>
+  'LazyScriptGoogleAdsense': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptGoogleAdsense.vue").default>
+  'LazyScriptIntercom': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptIntercom.vue").default>
+  'LazyScriptLemonSqueezy': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptLemonSqueezy.vue").default>
+  'LazyScriptLoadingIndicator': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptLoadingIndicator.vue").default>
+  'LazyScriptPayPalButtons': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalButtons.vue").default>
+  'LazyScriptPayPalMarks': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalMarks.vue").default>
+  'LazyScriptPayPalMessages': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptPayPalMessages.vue").default>
+  'LazyScriptStripePricingTable': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptStripePricingTable.vue").default>
+  'LazyScriptVimeoPlayer': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptVimeoPlayer.vue").default>
+  'LazyScriptYouTubePlayer': LazyComponent<typeof import("../../../../node_modules/@nuxt/scripts/dist/runtime/components/ScriptYouTubePlayer.vue").default>
+  'LazyNuxtWelcome': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/welcome.vue").default>
+  'LazyNuxtLayout': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-layout").default>
+  'LazyNuxtErrorBoundary': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-error-boundary.vue").default>
+  'LazyClientOnly': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/client-only").default>
+  'LazyDevOnly': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/dev-only").default>
+  'LazyServerPlaceholder': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/server-placeholder").default>
+  'LazyNuxtLink': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-link").default>
+  'LazyNuxtLoadingIndicator': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-loading-indicator").default>
+  'LazyNuxtTime': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-time.vue").default>
+  'LazyNuxtRouteAnnouncer': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-route-announcer").default>
+  'LazyNuxtImg': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-stubs").NuxtImg>
+  'LazyNuxtPicture': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-stubs").NuxtPicture>
+  'LazyNuxtLinkLocale': LazyComponent<typeof import("../../../../node_modules/@nuxtjs/i18n/dist/runtime/components/NuxtLinkLocale").default>
+  'LazySwitchLocalePathLink': LazyComponent<typeof import("../../../../node_modules/@nuxtjs/i18n/dist/runtime/components/SwitchLocalePathLink").default>
+  'LazyNuxtPage': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/pages/runtime/page-placeholder").default>
+  'LazyNoScript': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").NoScript>
+  'LazyLink': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Link>
+  'LazyBase': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Base>
+  'LazyTitle': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Title>
+  'LazyMeta': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Meta>
+  'LazyStyle': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Style>
+  'LazyHead': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Head>
+  'LazyHtml': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Html>
+  'LazyBody': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/head/runtime/components").Body>
+  'LazyNuxtIsland': LazyComponent<typeof import("../../../../node_modules/nuxt/dist/app/components/nuxt-island").default>
+}
+
+declare module 'vue' {
+  export interface GlobalComponents extends _GlobalComponents { }
+}
+
+export {}
