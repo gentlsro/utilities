70442758b441e118713b856a58ac5ffeaf3bde0e
Kylian, Jakub <jacob.kylian@gmail.com>
2026-01-15 23:28:44 +0100

    chore: up

--- shared/types/object-key.type.ts
+++ shared/types/object-key.type.ts
@@ -8,18 +8,23 @@ type Path<T extends string | number, U extends string> = U extends ''
   : `${T}${'' extends U ? '' : '.'}${U}`
 
 // NOTE It's possible to change the depth of the path by changing the number in the PathKeys type
-type PathKeys<T, D extends number = 3> = [D] extends [never]
+// SkipArrayIndex: when true, allows `roles.id` instead of `roles.${number}.id`
+type PathKeys<T, D extends number = 3, SkipArrayIndex extends boolean = false> = [D] extends [never]
   ? never
   : T extends Primitive
     ? ''
     : T extends Array<infer U>
-      ? PathKeys<U, Prev[D]> extends ''
-        ? `${number}`
-        : Path<`${number}`, PathKeys<U, Prev[D]>>
+      ? SkipArrayIndex extends true
+        // Skip array index - treat array as if accessing element directly
+        ? PathKeys<U, Prev[D], SkipArrayIndex>
+        // Include array index in path
+        : PathKeys<U, Prev[D], SkipArrayIndex> extends ''
+          ? `${number}`
+          : Path<`${number}`, PathKeys<U, Prev[D], SkipArrayIndex>>
       : T extends object
         ? {
             [K in keyof T]-?: K extends string | number
-              ? Path<K, PathKeys<T[K], Prev[D]>>
+              ? Path<K, PathKeys<T[K], Prev[D], SkipArrayIndex>>
               : never
           }[keyof T]
         : never
@@ -29,6 +34,17 @@ type Prev = [never, 0, 1, 2, 3, 4]
 /**
  * Extracts the keys of an object
  *
- * NOTE: No fucking idea how this works, all chatgpt...
+ * @template T - The object type to extract keys from
+ * @template SkipArrayIndex - When true, allows `roles.id` instead of `roles.${number}.id`
+ *
+ * @example
+ * // Default behavior (with array index)
+ * type Keys = ObjectKey<{ roles: { id: string }[] }>
+ * // Result: "roles" | `roles.${number}` | `roles.${number}.id`
+ *
+ * @example
+ * // Skip array index
+ * type Keys = ObjectKey<{ roles: { id: string }[] }, true>
+ * // Result: "roles" | "roles.id"
  */
-export type ObjectKey<T> = PathKeys<T>
+export type ObjectKey<T, SkipArrayIndex extends boolean = false> = PathKeys<T, 3, SkipArrayIndex>
