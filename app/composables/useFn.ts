7259ceb84ebb27fa037b70ec04dda15e51661020
Kylian, Jakub <jacob.kylian@gmail.com>
2026-01-22 09:04:56 +0100

    up

--- app/composables/useFn.ts
+++ app/composables/useFn.ts
@@ -101,6 +101,8 @@ export function useFn(options?: IUseFnOptions) {
     fnc: AsyncFunction<T>,
     options?: UseFnPayload<T>,
   ): Promise<T> {
+    console.log('👀 From utilities')
+
     const {
       fnId,
       merge,
