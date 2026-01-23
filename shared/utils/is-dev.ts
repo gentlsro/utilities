fa0199b27c5fc2d944a7cf1e8524310b2a8b2278
Kylian, Jakub <jacob.kylian@gmail.com>
2026-01-23 15:30:24 +0100

    up

--- shared/utils/is-dev.ts
+++ shared/utils/is-dev.ts
@@ -1,3 +1,16 @@
 export function isDev() {
-  return import.meta.dev
+  if (import.meta.dev) {
+    return true
+  }
+
+  const rC = useRuntimeConfig()
+  const env = rC.public.env
+
+  if (env === 'local') {
+    return true
+  } else if (env === 'development') {
+    return true
+  }
+
+  return false
 }
