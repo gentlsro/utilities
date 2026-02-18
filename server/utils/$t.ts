7c30d3fc95190c1d630609d4ee199cc2bd2d168f
Kylian, Jakub <jacob.kylian@gmail.com>
2026-02-18 20:50:43 +0100

    chore: up

--- server/utils/$t.ts
+++ server/utils/$t.ts
@@ -1,19 +1,15 @@
-import type { NamedValue, TranslateOptions } from '#i18n'
+import type { NamedValue } from '#i18n'
 
 /**
  * i18n.t
  */
-export function $t(key: string, pluralOrNamed?: NamedValue, options?: TranslateOptions): string
-export function $t(key: string, pluralOrNamed?: string | number, options?: TranslateOptions): string
-export function $t(key: string, pluralOrNamed?: string | number | NamedValue, options?: TranslateOptions): string {
+export function $t(key: string, pluralOrNamed?: NamedValue): string
+export function $t(key: string, pluralOrNamed?: string | number): string
+export function $t(key: string, pluralOrNamed?: string | number | NamedValue): string {
   if (!pluralOrNamed) {
     return key
   }
 
-  if (!options) {
-    return `${key}, ${pluralOrNamed}`
-  }
-
   // NOTE: This is a dummy function that just returns the key for server usage
-  return `${key}, ${pluralOrNamed}, ${options}`
+  return `${key}, ${pluralOrNamed}`
 }
