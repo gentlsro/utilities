36cbf1350c6e5f4d8ce64cd3dc06bc557c921f52
Kylian, Jakub <jacob.kylian@gmail.com>
2026-08-04 01:00:01 +0200

    up?

--- shims.d.ts
+++ shims.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable ts/consistent-type-definitions */
 
 interface ImportMetaEnv {
-  readonly VITE_MONOREPO: string
+  readonly VITE_MONOREPO?: string
   readonly VITE_INSTALL_LAYER_DEPS: string
   readonly NUXT_PUBLIC_ENV: string
   readonly NUXT_PUBLIC_FILES_HOST: string
